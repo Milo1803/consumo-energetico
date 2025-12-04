78f2f214824f60dcf9e185a96b8adf1fd1c34bf5
Brayan Riascos <b.riascos@utp.edu.co>
2025-12-04 03:52:38 -0500

    Update config.py to use environment variables for Railway

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import mysql.connector
 def conectar():
     try:
         connection = mysql.connector.connect(
-            host="mysql.railway.internal",
-            port=3306,
-            user="root",
-            password="NnKKueYrSzNYFbgZuAZHKWKrdeeHJeox",
-            database="railway"
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT", 3306)),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
+        print("✔ Conexión exitosa a MySQL")
         return connection
     except Exception as e:
-        print("Error al conectar:", e)
+        print("❌ Error al conectar:", e)
         return None
-
